46ae55e4fd9839ad30907a5dc93fea85c76b13d5
wasaw <merenkovalexander@gmail.com>
2021-09-23 15:40:13 +0300

    add animation and CoreData

--- ToDo/Control/HomeController.swift
+++ ToDo/Control/HomeController.swift
@@ -6,17 +6,36 @@
 //
 
 import UIKit
+import CoreData
 
-class HomeController: UIViewController {
+protocol GetTextProtocol {
+    func sendText(text: String, index: Int)
+}
+
+class HomeController: UIViewController, GetTextProtocol {
     private let tableView = UITableView()
     private let refreshControl = UIRefreshControl()
     private var cellCount = 1
+    private var haveDataInCore = false
+    private var arrNote = [String]()
     private let inserts = UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0)
+                        
+    var context: NSManagedObjectContext? {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return nil
+        }
+        let context = appDelegate.persistentContainer.viewContext
+        return context
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
         tableView.tableFooterView = UIView()
+        
+        if checkValueInDatabase() {
+            haveDataInCore = true
+        }
     }
     
     func configureUI() {
@@ -40,17 +59,104 @@ class HomeController: UIViewController {
         tableView.refreshControl = refreshControl
         refreshControl.isHidden = true 
         refreshControl.addTarget(self, action: #selector(addNewCell), for: .valueChanged)
-        
-        tableView.backgroundColor = .lightGray
+                
+//        tableView.backgroundColor = .systemBackground
+        tableView.backgroundColor = .init(red: 11/255, green: 19/255, blue: 43/255, alpha: 1)
     }
     
     @objc func addNewCell() {
         cellCount += 1
-        tableView.reloadData()
+        arrNote.append("")
+        UIView.animate(withDuration: 0.5) {
+            self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .top)
+        }
         self.refreshControl.endRefreshing()
+        
+    }
+    
+    func sendText(text: String, index: Int) {
+        if arrNote.count == index {
+            saveValue(text: text, index: index)
+        } else {
+            updateValue(text: text, index: index)
+        }
+    }
+    
+    func checkValueInDatabase() -> Bool {
+        if !readValue() {
+            return false
+        }
+        return true
+    }
+    
+    func saveValue(text: String, index: Int) {
+        guard let context = context else { return }
+        guard let entity = NSEntityDescription.entity(forEntityName: "List", in: context) else { return }
+        
+        let newValue = NSManagedObject(entity: entity, insertInto: context)
+        
+        newValue.setValue(index, forKey: "idCell")
+        newValue.setValue(text, forKey: "textNote")
+        arrNote.append(text)
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func readValue() -> Bool {
+        guard let context = context else { return false }
+        let featchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "List")
+        
+        do {
+            let result = try context.fetch(featchRequest)
+            if result.count == 0 {
+                return false
+            }
+            arrNote = []
+            for data in result {
+                if let data = data as? NSManagedObject {
+                    arrNote.append(data.value(forKey: "textNote") as? String ?? "")
+                }
+            }
+            cellCount = result.count
+        } catch {
+            print(error)
+        }
+        return true
+    }
+    
+    func updateValue(text: String, index: Int) {
+        guard let context = context else { return }
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "List")
+        fetchRequest.predicate = NSPredicate(format: "idCell == %@", "\(index)")
+        do {
+            let result = try context.fetch(fetchRequest)
+            guard let value = result.first as? NSManagedObject else { return }
+            value.setValue(text, forKey: "textNote")
+            try context.save()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func deleteValue(text: String) {
+        guard let context = context else { return }
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "List")
+        fetchRequest.predicate = NSPredicate(format: "textNote = %@", text)
+        do {
+            let result = try context.fetch(fetchRequest)
+            guard let value = result.first as? NSManagedObject else { return }
+            context.delete(value)
+            try context.save()
+        } catch {
+            print(error)
+        }
     }
 }
 
+//  MARK: - extension
 extension HomeController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cellCount
@@ -59,19 +165,40 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifire, for: indexPath) as! HomeTableViewCell
         cell.selectionStyle = .none
+        cell.delegateText = self
+
+        if haveDataInCore {
+//            print out note in reverse order
+            let index = arrNote.count - indexPath.row - 1
+            cell.setTextFieldText(title: arrNote[index], index: index)
+            if cellCount - 1 == indexPath.row{
+                haveDataInCore = false
+            }
+        }else {
+            cell.setTextFieldText(title: "", index: arrNote.count)
+        }
         
         return cell
     }
     
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             tableView.beginUpdates()
-            tableView.deleteRows(at: [indexPath], with: .left)
             cellCount -= 1
+           
+//            print out note in reverse order
+            let index = arrNote.count - indexPath.row - 1
+            print("cellCount \(cellCount), arrNote.count \(arrNote.count), index \(index)")
+            if arrNote.count != 0 {
+                deleteValue(text: arrNote[index])
+                arrNote.remove(at: index)
+            }
+            tableView.deleteRows(at: [indexPath], with: .left)
+        
 //            tableView.reloadData()
             tableView.endUpdates()
         }
@@ -81,5 +208,3 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
         
     }
 }
-
-
--- ToDo/View/HomeTableViewCell.swift
+++ ToDo/View/HomeTableViewCell.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class HomeTableViewCell: UITableViewCell, UITextFieldDelegate {
+    
     static let identifire = "HomeTableViewCell"
     
     static func nib() -> UINib {
          return UINib(nibName: "HomeTableViewCell", bundle: nil)
     }
+    
+    var delegateText: GetTextProtocol?
     
     private let textFieldCell: UITextField = {
         let textField = UITextField()
@@ -30,8 +33,7 @@ class HomeTableViewCell: UITableViewCell, UITextFieldDelegate {
         super.awakeFromNib()
         textFieldCell.delegate = self
 //        backgroundColor = UIColor.blue
-        backgroundColor = UIColor.systemOrange
-//        layer.borderWidth = 1
+        backgroundColor = UIColor.init(red: 91/255, green: 192/255, blue: 190/255, alpha: 1)
         layer.borderColor = UIColor.black.cgColor
         addSubview(textFieldCell)
         textFieldCell.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
@@ -40,13 +42,19 @@ class HomeTableViewCell: UITableViewCell, UITextFieldDelegate {
         textFieldCell.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         textFieldCell.leftView = UIView(frame: CGRect(x: 0, y: 10, width: 10, height: 10))
         textFieldCell.leftViewMode = .always
-        
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textFieldCell.endEditing(true)
+        delegateText?.sendText(text: textFieldCell.text!, index: textFieldCell.tag)
+        print(textFieldCell.tag)
+        return false
+    }
+    
+    internal func setTextFieldText(title: String, index: Int) {
+        self.textFieldCell.text = title
+        self.textFieldCell.tag = index
+        print(textFieldCell.tag)
+    }
 }
+
